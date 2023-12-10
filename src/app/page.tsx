c7d08c506c2740580b2110990e3228bd8125fc77
qutluq <kootlook.applications@gmail.com>
2023-12-10 10:05:40 +0600

    feat: add temp content to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Icon } from '@/components/Icon'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Main
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <div className="absolute left-0 top-0 flex w-full flex-row justify-center py-6">
+        <Icon
+          glyph="logo"
+          size={{ width: 300, height: 54 }}
+          className="text-gray-900"
+        />
+      </div>
+      <span className="text-xl">Сайт ясилиш җәриянида...</span>
     </main>
   )
 }
